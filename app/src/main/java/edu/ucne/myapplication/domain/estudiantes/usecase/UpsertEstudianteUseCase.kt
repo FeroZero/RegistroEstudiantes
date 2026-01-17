9b909ffb436652375a17ecde39ade4efb459e2f9
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-17 14:24:32 -0400

    Se agrego el caso de uso Upsert para Estudiante, llamado UpsertEstudianteUseCase.

---
+++ app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/UpsertEstudianteUseCase.kt
@@ -0,0 +1,24 @@
+package edu.ucne.myapplication.domain.estudiantes.usecase
+
+import edu.ucne.myapplication.domain.estudiantes.model.Estudiante
+import edu.ucne.myapplication.domain.estudiantes.repository.EstudianteRepository
+
+class UpsertEstudianteUseCase(
+    private val repository: EstudianteRepository
+) {
+    suspend operator fun invoke(estudiante: Estudiante): Result<Int> {
+        val nombresResult = validateNombres(estudiante.nombres)
+        if (!nombresResult.isValid) {
+            return Result.failure(IllegalArgumentException(nombresResult.errorMessage))
+        }
+        val emailResult = validateEmail(estudiante.email)
+        if (!emailResult.isValid) {
+            return Result.failure(IllegalArgumentException(nombresResult.errorMessage))
+        }
+        val edadResult = validateEdad(estudiante.edad)
+        if (!edadResult.isValid) {
+            return Result.failure(IllegalArgumentException(nombresResult.errorMessage))
+        }
+        return runCatching { repository.upsert(estudiante) }
+    }
+}
