13209558496d779dddcb755a2fdbfee551e40044
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-17 14:23:46 -0400

    Se crearon los caso de uso, y se agrego EstudianteValidations como primer archivo.

---
+++ app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/EstudianteValidations.kt
@@ -0,0 +1,25 @@
+package edu.ucne.myapplication.domain.estudiantes.usecase
+
+data class ValidationResult(
+    val isValid: Boolean,
+    val errorMessage: String? = null
+)
+
+fun validateNombres(value: String): ValidationResult {
+    if (value.isBlank()) return ValidationResult(false, "Campo Obligatorio")
+    return ValidationResult(true)
+}
+
+fun validateEmail(value: String): ValidationResult {
+    if (value.isBlank()) return ValidationResult(false, "Campo Obligatorio")
+    if (!value.contains("@")) return ValidationResult(false, "Email Invalido")
+    return ValidationResult(true)
+}
+
+fun validateEdad(value: Int?): ValidationResult {
+    if (value == null) return ValidationResult(false, "Campo Obligatorio")
+    if (value <=0) return ValidationResult(false, "Edad Invalida")
+    if (value > 100) return ValidationResult(false, "Edad Invalida")
+    return ValidationResult(true)
+
+}
