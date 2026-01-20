78f07e69673a2d2c23bd4328eab8047490d3456e
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-20 00:04:38 -0400

    GetEstudianteUseCase.kt Arreglado.

--- app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/GetEstudianteUseCase.kt
+++ app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/GetEstudianteUseCase.kt
@@ -2,8 +2,9 @@ package edu.ucne.myapplication.domain.estudiantes.usecase
 
 import edu.ucne.myapplication.domain.estudiantes.model.Estudiante
 import edu.ucne.myapplication.domain.estudiantes.repository.EstudianteRepository
+import javax.inject.Inject
 
-class GetEstudianteUseCase(
+class GetEstudianteUseCase @Inject constructor(
     private val repository: EstudianteRepository
 ) {
     suspend operator fun invoke(id: Int): Estudiante? = repository.getEstudiante(id)
