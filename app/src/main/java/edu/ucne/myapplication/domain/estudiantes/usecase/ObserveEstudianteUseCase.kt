967cc04594c84c878a24bdb8bcf889f70254b22e
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-20 00:04:55 -0400

    ObserveEstudianteUseCase.kt Arreglado.

--- app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/ObserveEstudianteUseCase.kt
+++ app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/ObserveEstudianteUseCase.kt
@@ -3,8 +3,9 @@ package edu.ucne.myapplication.domain.estudiantes.usecase
 import edu.ucne.myapplication.domain.estudiantes.model.Estudiante
 import edu.ucne.myapplication.domain.estudiantes.repository.EstudianteRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class ObserveEstudianteUseCase(
+class ObserveEstudianteUseCase @Inject constructor(
     private val repository: EstudianteRepository
 ) {
     operator fun invoke(): Flow<List<Estudiante>> = repository.observeEstudiante()
