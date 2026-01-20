1d6f496b04749e2ce13b4c31dc92341f2a22d3f4
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-20 00:00:46 -0400

    AppModule actualizado.

--- app/src/main/java/edu/ucne/myapplication/di/AppModule.kt
+++ app/src/main/java/edu/ucne/myapplication/di/AppModule.kt
@@ -2,24 +2,42 @@ package edu.ucne.myapplication.di
 
 import android.content.Context
 import androidx.room.Room
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.myapplication.data.estudiantes.local.database.EstudianteDb
+import edu.ucne.myapplication.data.estudiantes.local.repositoryimpl.EstudianteRepositoryImpl
+import edu.ucne.myapplication.domain.estudiantes.repository.EstudianteRepository
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-object AppModule {
-    @Provides
+abstract class AppModule {
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideEstudianteDb(@ApplicationContext appContext: Context) =
+            Room.databaseBuilder(
+                appContext,
+                klass = EstudianteDb::class.java,
+                name = "Estudiante.db"
+            ).fallbackToDestructiveMigration()
+                .build()
+
+        @Provides
+        fun provideEstudianteDao(db: EstudianteDb) = db.estudianteDao()
+    }
+    @Binds
     @Singleton
-    fun provideEstudianteDb(@ApplicationContext appContext: Context) =
-        Room.databaseBuilder(
-            appContext,
-            klass = EstudianteDb::class.java,
-            name = "Estudiante.db"
-        ).fallbackToDestructiveMigration()
-            .build()
+    abstract fun bindEstudianteRepository(
+        impl: EstudianteRepositoryImpl
+    ): EstudianteRepository
 }
+
+
+
+
