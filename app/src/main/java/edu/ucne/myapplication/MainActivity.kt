2cf24cdbf720b36005a46e10b91794c150466abb
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-20 00:05:26 -0400

    MainActivity.kt simplificado.

--- app/src/main/java/edu/ucne/myapplication/MainActivity.kt
+++ app/src/main/java/edu/ucne/myapplication/MainActivity.kt
@@ -4,26 +4,61 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.myapplication.presentation.estudiantes.EstudianteScreen
+import edu.ucne.myapplication.presentation.estudiantes.EstudianteUiEvent
+import edu.ucne.myapplication.presentation.estudiantes.EstudianteUiViewModel
+import edu.ucne.myapplication.presentation.estudiantes.list.EstudianteListScreen
 import edu.ucne.myapplication.ui.theme.RegistroEstudianteTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RegistroEstudianteTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                val viewModel: EstudianteUiViewModel = hiltViewModel()
+                val state by viewModel.state.collectAsStateWithLifecycle()
+
+                NavHost(navController = navController, startDestination = "Lista") {
+                    composable("Lista") {
+                        EstudianteListScreen(
+                            state = state,
+                            onEvent = viewModel::onEvent,
+                            onAddEstudiante = {
+                                viewModel.onEvent(EstudianteUiEvent.New)
+                                navController.navigate("Registro")
+                            },
+                            onEditEstudiante = { id ->
+                                viewModel.onEvent(EstudianteUiEvent.Edit(id))
+                                navController.navigate("Registro")
+                            }
+                        )
+                    }
+                    composable("Registro") {
+                        EstudianteScreen(
+                            viewModel = viewModel,
+                            onNavigateBack = {
+                                viewModel.resetSavedStatus()
+                                navController.navigateUp()
+                            }
+                        )
+                    }
                 }
             }
         }
