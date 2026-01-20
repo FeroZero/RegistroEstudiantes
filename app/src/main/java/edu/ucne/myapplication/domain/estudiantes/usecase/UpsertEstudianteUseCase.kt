bf7aaf566c40fc2e9553d9ff10d9884b0e8af82f
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-20 00:05:15 -0400

    UpsertEstudianteUseCase.kt Arreglado.

--- app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/UpsertEstudianteUseCase.kt
+++ app/src/main/java/edu/ucne/myapplication/domain/estudiantes/usecase/UpsertEstudianteUseCase.kt
@@ -2,8 +2,9 @@ package edu.ucne.myapplication.domain.estudiantes.usecase
 
 import edu.ucne.myapplication.domain.estudiantes.model.Estudiante
 import edu.ucne.myapplication.domain.estudiantes.repository.EstudianteRepository
+import javax.inject.Inject
 
-class UpsertEstudianteUseCase(
+class UpsertEstudianteUseCase @Inject constructor(
     private val repository: EstudianteRepository
 ) {
     suspend operator fun invoke(estudiante: Estudiante): Result<Int> {
