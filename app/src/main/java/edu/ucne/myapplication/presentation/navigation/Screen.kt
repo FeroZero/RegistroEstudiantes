2a0ea1a469b2b3a62014277d1f8e781669168974
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-30 03:29:29 -0400

    Se agrego el Screen para las pantallas de Estudiante.

---
+++ app/src/main/java/edu/ucne/myapplication/presentation/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package edu.ucne.myapplication.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object EstudianteList : Screen()
+    @Serializable
+    data class Estudiante(val id: Int) : Screen()
+}
