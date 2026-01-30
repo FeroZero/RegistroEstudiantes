6a89add030dc1cf0dea6ae16c7ec6475792a1677
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2026-01-30 03:29:05 -0400

    Se creo el NavHost del proyecto.

---
+++ app/src/main/java/edu/ucne/myapplication/presentation/navigation/myapplicationNavHost.kt
@@ -0,0 +1,48 @@
+package edu.ucne.myapplication.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import edu.ucne.myapplication.presentation.estudiantes.EstudianteScreen
+import edu.ucne.myapplication.presentation.estudiantes.EstudianteUiViewModel
+import edu.ucne.myapplication.presentation.estudiantes.list.EstudianteListScreen
+
+@Composable
+fun myapplicacionNavHost(
+    navHostController: NavHostController
+) {
+    val viewModel: EstudianteUiViewModel = hiltViewModel()
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.EstudianteList
+    ) {
+        composable<Screen.EstudianteList>{
+            EstudianteListScreen(
+                onAddEstudiante = {
+                    navHostController.navigate(Screen.Estudiante(0))
+                },
+                onEditEstudiante = { id ->
+                    navHostController.navigate(Screen.Estudiante(id))
+                },
+                onEvent = { event ->
+                    viewModel.onEvent(event)
+                },
+                state = state
+            )
+        }
+        composable<Screen.Estudiante> {
+            EstudianteScreen(
+                viewModel = viewModel,
+                onNavigateBack = {
+                    navHostController.navigateUp()
+                },
+            )
+        }
+    }
+}
